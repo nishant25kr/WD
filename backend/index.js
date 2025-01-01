47f9225bdd249ec853917affe06171a3c5120100
Nishant kumar <nishant927472@gmail.com>
2025-01-01 12:51:11 +0530

    mini project2

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express=require("express");
 const path=require("path")
 const app = express()
 const fs=require('fs');
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,14 +10,19 @@ app.set('view engine','ejs')
 app.get('/', function (req, res) {
   fs.readdir(`./files`,function(err,files){
     res.render("index",{files: files})
-
   })
 })
+
 app.post('/create', function (req, res) {
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
       res.redirect('/')
   });
+})
 
+app.get('/file/:filename',function(req, res){
+  fs.readFile(`./files/${req.params.filename}`,"utf-8" ,function(err,filedata){
+    res.render('show', {filename: req.params.filename , filedata: filedata});
+  })
 })
 
 app.listen(3000)
